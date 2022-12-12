040ffdbf5ee3ee98a99cdd07c30378e1213bb5ec
Marius <Marius.Supercode@gmail.com>
2022-12-12 14:52:41 +0100

    added Router DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Navigation from './components/Navigation/Navigation';
+import SearchGenre from './pages/SearchGenre/SearchGenre';
+import Detail from './pages/Detail/Detail';
+import StartPage from './pages/StartPage/StartPage';
 
 function App() {
   return (
     <div className="App">
+      <Router>
 
+        <Routes>
+          <Route path="/" element={<><StartPage /></>} />
+          <Route path="/home" element={<><Home /><Navigation /></>} />
+          <Route path="/genre/:genreID" element={<><SearchGenre /><Navigation /></>} />
+          <Route path="/details/:movieID/:movieName" element={<><Detail /><Navigation /></>} />
+          {/* <Route path="/details/:movieID/:movieName" element={<><Detail /></>} /> */}
+
+        </Routes>
+      </Router>
     </div>
   );
 }
