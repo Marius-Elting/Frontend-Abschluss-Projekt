7b33a58bb422fb53e28083160889b8f9b58bc73b
WingsOfFury <akcengiz@gmx.de>
2022-12-13 14:53:46 +0100

    GenreCard

--- src/components/GenreCard/GenreCard.js
+++ src/components/GenreCard/GenreCard.js
@@ -9,13 +9,26 @@ function GenreCard({ data }) {
     // console.log(cbd);
 
     return (
-        <Link to={`/details/${data.id}/${data.original_title}`}>
-            <img src={`https://image.tmdb.org/t/p/w300/${data.poster_path}`} alt={data.originale_title}></img>
-            <h3>{data.original_title}</h3>
-            <p>{data.vote_average}</p>
-            <p>{date.getFullYear()}</p>
-            <p>{genre.name}</p>
-        </Link>
+        <Link className="genreCard" to={`/details/${data.id}/${data.original_title}`}>
+
+            <img className="genreCard-poster" src={`https://image.tmdb.org/t/p/w300/${data.poster_path}`} alt={data.originale_title}></img>
+            {/* <img className="genreCard-bookmark" src='./assets/icons/Star.svg'></img> */}
+
+            <section className="genreCard-information">
+
+                <article className="genreCard-movieTitle">
+                    <h3 className="genreCard-title">{data.original_title}</h3>
+                    <p className="genreCard-rating">{data.vote_average}</p>
+                    <p className="genreCard-speichern">p</p>
+                </article>
+
+                <article className="genreCard-movieText">
+                    <p className="genreCard-releaseYear">{date.getFullYear()} {genre?.name}</p>
+
+                </article>
+            </section>
+
+        </Link >
     );
 }
 
