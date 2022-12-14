0c9f7566d45cb6d10073f9c50600546fd1787618
Marius <Marius.Supercode@gmail.com>
2022-12-14 10:03:01 +0100

    improved Styling

--- src/components/GenreCard/GenreCard.js
+++ src/components/GenreCard/GenreCard.js
@@ -2,36 +2,33 @@ import './GenreCard.css';
 import GenreList from './GenreList.json';
 import { Link } from 'react-router-dom';
 
-function GenreCard({ data }) {
+function GenreCard({ data, index }) {
     const date = new Date(data.release_date);
     console.log(GenreList.genres);
     let genre = GenreList.genres.find((genre) => genre.id === data.genre_ids[0]);
     // console.log(cbd);
 
     return (
-        <Link className="genreCard" to={`/details/${data.id}/${data.original_title}`}>
+        <Link key={index} className="genreCard" to={`/details/${data.id}/${data.original_title}`}>
 
             <img className="genreCard-poster" src={`https://image.tmdb.org/t/p/w300/${data.poster_path}`} alt={data.originale_title}></img>
             {/* <img className="genreCard-bookmark" src='./assets/icons/Star.svg'></img> */}
 
             <section className="genreCard-information">
-
                 <article className="genreCard-movieTitle">
                     <h3 className="genreCard-title">{data.original_title}</h3>
-
                     <article>
                         <p className="genreCard-rating">{data.vote_average}</p>
                         <p className="genreCard-save">p</p>
                     </article>
                 </article>
-
                 <article className="genreCard-movieText">
-                    <p className="genreCard-releaseYear">{date.getFullYear()}   {genre?.name}</p>
+                    <p className="genreCard-releaseYear">{date.getFullYear()} &nbsp;  {genre?.name}</p>
 
                 </article>
             </section>
 
-        </Link >
+        </Link>
     );
 }
 
