de142c0dfee0dd00053167ef60579ed4a35e97e3
Marius <Marius.Supercode@gmail.com>
2022-12-15 19:25:56 +0100

    fixed issue

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import GenreCard from "../../components/GenreCard/GenreCard";
 import './Favorites.css';
 import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
