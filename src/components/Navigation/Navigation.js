cf2492c8150f243c0701d293cc8467c37c7f4b8f
Marius <Marius.Supercode@gmail.com>
2022-12-15 11:30:53 +0100

    styling improved

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,23 +3,25 @@ import Profile from '../../assets/icons/Profile.svg';
 import Bookmark from '../../assets/icons/Bookmark.svg';
 import Download from '../../assets/icons/Download.svg';
 import Home from '../../assets/icons/Home.svg';
+import GrayHome from '../../assets/icons/GrayHome.svg';
 import { Link } from 'react-router-dom';
+import RedBookmark from '../../assets/icons/Bookmark-svg-red.svg';
 
 
-function Navigation() {
+function Navigation({ page }) {
+    console.log(page);
     return (
         <div className='navigation'>
             <Link to="/home">
-                <img className='navLinkToHome' alt="Home Button" src={Home}></img>
+                <img className='navLinkToHome' alt="Home Button" src={page !== "home" ? GrayHome : Home}></img>
             </Link>
             <Link to="/favorites">
-                <img alt="Bookmark Button" src={Bookmark}></img>
+                <img alt="Bookmark Button" src={page === "favo" ? RedBookmark : Bookmark}></img>
             </Link>
             <img alt="Download Button" src={Download}></img>
             <img alt="Profile Button" src={Profile}></img>
         </div>
     );
-
 }
 
 export default Navigation;
