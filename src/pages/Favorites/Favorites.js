9180798b36a7855d82b52908594bd463554e66b3
miradonis <miradonis@web.de>
2022-12-15 15:31:17 +0100

    added styling for favorites

--- src/pages/Favorites/Favorites.js
+++ src/pages/Favorites/Favorites.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import GenreCard from "../../components/GenreCard/GenreCard";
 import './Favorites.css';
+import ArrowLeft from '../../assets/icons/arrowLeft.png';
 
 function Favorites({ Favorites }) {
+    let navigate = useNavigate();
 
     return (
         <div className="Favorites-Wrapper">
-            <h1>Deine Favoriten</h1>
+            <div className="FavoritesBtnAndHeading">
+                <button className='favoritesBackButton' onClick={() => navigate(-1)}>
+                    <img alt='img' src={ArrowLeft}></img>
+                </button>
+                <h1>Deine Favoriten</h1>
+            </div>
             {Favorites.map((data) => {
-
                 return (
                     <GenreCard data={data} />
                 );
